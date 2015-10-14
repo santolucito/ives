086bdcbfabcf78468df6f8f48f0cde6c0a716ee0
Marvin Qian <fueledbymarvin@gmail.com>
2015-10-14 01:37:59 -0400

    Export ExampleGen.Gen as a library
    This allows the scripts generated by the Exampler executable to import
    that library so Exampler can generate examples.

--- Ives/ExampleGen/Exampler.hs
+++ Ives/ExampleGen/Exampler.hs
@@ -1,9 +1,13 @@
+import Data.List
+import Data.Maybe
 import System.IO
 import System.IO.Temp
 import System.FilePath
 import System.Directory
 import System.Environment
-import Data.List
+import System.Process
+import System.Exit
+import Control.Exception
 
 import System.INotify
 
@@ -29,8 +33,19 @@ handler _ DeletedSelf       = putStrLn "File was deleted"
 exampler :: FilePath -> IO ()
 exampler path = do
   temp <- createTemp path
-  putStrLn temp
-  removeFile temp
+  (e, out, err) <- readProcessWithExitCode "ghc" ["-fhpc", temp] ""
+  case e of
+    ExitSuccess -> putStrLn out
+    ExitFailure _ -> do
+      putStrLn err
+      exitFailure
+
+  let program = takeBaseName temp
+  let executable = joinPath [".", program]
+  h <- openFile (addExtension (takeBaseName path) "examples") WriteMode
+  findExamples executable h
+  hClose h
+  cleanup program
 
 createTemp :: FilePath -> IO String
 createTemp path = do
@@ -38,18 +53,32 @@ createTemp path = do
   -- remove byte order mark
   let func = dropWhile (\c -> c == '\65279') contents
   let funcName = takeWhile (\c -> c /= ' ') func
-  (temp, handle) <- openTempFile dir fileName
-  hPutStrLn handle   "import System.Environment"
-  hPutStrLn handle   "import Ives.ExampleGen.Gen"
-  hPutStrLn handle   ""
-  hPutStrLn handle   "main :: IO ()"
-  hPutStrLn handle   "main = do"
-  hPutStrLn handle   "  n:size:_ <- getArgs"
-  hPutStrLn handle $ "  genExamples " ++ funcName ++ " (read n) (read size)"
-  hPutStrLn handle   ""
-  hPutStrLn handle   func
-  hClose handle
+  (temp, h) <- openTempFile dir fileName
+  hPutStrLn h   "import System.Environment"
+  hPutStrLn h   "import Ives.ExampleGen.Gen"
+  hPutStrLn h   ""
+  hPutStrLn h   "main :: IO ()"
+  hPutStrLn h   "main = do"
+  hPutStrLn h   "  n:size:_ <- getArgs"
+  hPutStrLn h $ "  genExamples " ++ funcName ++ " (read n) (read size)"
+  hPutStrLn h   ""
+  hPutStrLn h   func
+  hClose h
   return temp
   where
-    dir = takeDirectory . takeDirectory . takeDirectory $ path
+    dir = takeDirectory path
     fileName = takeFileName path
+
+findExamples :: FilePath -> Handle -> IO ()
+findExamples executable h = do
+  example <- readProcess executable ["1", "10"] ""
+  hPutStrLn h example
+
+cleanup :: FilePath -> IO ()
+cleanup program = do
+  removeFile program
+  removeFile $ addExtension program "hs"
+  removeFile $ addExtension program "hi"
+  removeFile $ addExtension program "o"
+  removeFile $ addExtension program "tix"
+  removeDirectoryRecursive ".hpc"
