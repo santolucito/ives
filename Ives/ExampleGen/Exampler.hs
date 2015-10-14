2370eb61ea9d9fc1a7ec13ab6afaf9afa8a9eec0
Marvin Qian <fueledbymarvin@gmail.com>
2015-10-14 15:43:54 -0400

    Finish porting ruby script to haskell

--- Ives/ExampleGen/Exampler.hs
+++ Ives/ExampleGen/Exampler.hs
@@ -30,6 +30,12 @@ handler path (Modified _ _) = exampler path
 handler _ (MovedSelf _)     = putStrLn "File was moved"
 handler _ DeletedSelf       = putStrLn "File was deleted"
 
+data Report = Report { coveredExpr :: Int
+                     , totalExpr :: Int
+                     , coveredAlt :: Int
+                     , totalAlt :: Int
+                     } deriving (Show)
+
 exampler :: FilePath -> IO ()
 exampler path = do
   temp <- createTemp path
@@ -43,9 +49,10 @@ exampler path = do
   let program = takeBaseName temp
   let executable = joinPath [".", program]
   h <- openFile (addExtension (takeBaseName path) "examples") WriteMode
-  findExamples executable h
+  findExamples executable h defaultReport
   hClose h
   cleanup program
+    where defaultReport = Report 0 1 0 1
 
 createTemp :: FilePath -> IO String
 createTemp path = do
@@ -69,10 +76,36 @@ createTemp path = do
     dir = takeDirectory path
     fileName = takeFileName path
 
-findExamples :: FilePath -> Handle -> IO ()
-findExamples executable h = do
-  example <- readProcess executable ["1", "10"] ""
-  hPutStrLn h example
+genReport :: FilePath -> IO Report
+genReport executable = do
+  report <- readProcess "hpc" ["report", "--include=Main", executable] ""
+  let reportLines = lines report
+  let expressions = reportLines!!0
+  let alternatives = reportLines!!5
+  let coveredExpr = read $ takeWhile (/= '/') $ drop 1 $ dropWhile (/= '(') expressions
+  let totalExpr = read $ takeWhile (/= ')') $ drop 1 $ dropWhile (/= '/') expressions
+  let coveredAlt = read $ takeWhile (/= '/') $ drop 1 $ dropWhile (/= '(') alternatives
+  let totalAlt = read $ takeWhile (/= ')') $ drop 1 $ dropWhile (/= '/') alternatives
+  return $ Report coveredExpr totalExpr coveredAlt totalAlt
+
+isCovered :: Report -> Bool
+isCovered (Report ce te ca ta) = ce == te && ca == ta
+
+hasImproved :: Report -> Report -> Bool
+hasImproved (Report cePrev _ caPrev _) (Report ce _ ca _) = ce > cePrev || ca > caPrev
+  
+findExamples :: FilePath -> Handle -> Report -> IO ()
+findExamples executable h prevReport
+  | isCovered prevReport = return ()
+  | otherwise = do
+      example <- readProcess executable ["1", "10"] ""
+      report <- genReport executable
+      if hasImproved prevReport report
+        then do
+        hPutStrLn h example
+        putStrLn example
+        else return ()
+      findExamples executable h report
 
 cleanup :: FilePath -> IO ()
 cleanup program = do
