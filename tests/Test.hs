d507c0c1f4465d7c45bcf4cd2de8248f33e54ef6
Marvin Qian <fueledbymarvin@gmail.com>
2015-11-14 21:12:42 -0500

    Template haskell to make functions concrete

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,28 +1,33 @@
-﻿{-# LANGUAGE LambdaCase #-}
+﻿{-# LANGUAGE TemplateHaskell #-}
+
+import Conc
 import Ives.ExampleGen.Gen
-import Data.Typeable
 import Control.Monad
+import Language.Haskell.TH
 
 main :: IO ()
 main = do
-  example <- genExample (map :: (Int -> Int) -> [Int] -> [Int]) 10
-  let (a0, a1, r) = extract (arguments example) (result example)
-  print (a0 1)
-  print a1
-  print r
+  example <- genExample ($(send "filter") :: $(test "filter")) 10
+  print example
 
-extract :: [AnyArbitrary] -> AnyExampleable -> ((Int -> Int), [Int], [Int])
-extract args res = (a0, a1, r)
-  where a0 = case args!!0 of
-          MkAA a ->
-            case cast a of
-            Just a -> a :: Int -> Int
-        a1 = case args!!1 of
-          MkAA a ->
-            case cast a of
-            Just a -> a :: [Int]
-        r = case res of
-          MkAE a ->
-            case cast a of
-            Just a -> a :: [Int]
+--   example <- genExample (map :: (Int -> Int) -> [Int] -> [Int]) 10
+--   let (a0, a1, r) = extract (arguments example) (result example)
+--   print (a0 1)
+--   print a1
+--   print r
+
+-- extract :: [AnyArbitrary] -> AnyExampleable -> ((Int -> Int), [Int], [Int])
+-- extract args res = (a0, a1, r)
+--   where a0 = case args!!0 of
+--           MkAA a ->
+--             case cast a of
+--             Just a -> a :: Int -> Int
+--         a1 = case args!!1 of
+--           MkAA a ->
+--             case cast a of
+--             Just a -> a :: [Int]
+--         r = case res of
+--           MkAE a ->
+--             case cast a of
+--             Just a -> a :: [Int]
 
