e3d996c04879f213e8c9d54a3bcc728276d11e14
Marvin Qian <fueledbymarvin@gmail.com>
2015-11-30 22:18:32 -0500

    Use context when generating concrete function type

--- tests/Foo.hs
+++ tests/Foo.hs
@@ -1,6 +1,11 @@
-{-# LANGUAGE LambdaCase #-}
+module Foo (doh) where
 
-fooMap :: (a -> b) -> [a] -> [b]
-fooMap f = \case
-  [] -> []
-  l -> map f l 
+class Bar a where
+  derp :: a -> String
+
+instance Bar (a -> b) where
+  derp a = "derp"
+
+doh :: (Show a, Bar b) => a -> b -> String
+doh a b = show a ++ derp b
+
