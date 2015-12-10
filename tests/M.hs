a40a1d9e639b6ceb0f7166ddba6cb09c35a2bbd4
Marvin Qian <fueledbymarvin@gmail.com>
2015-12-09 19:30:36 -0500

    Simplify file update notification

--- tests/M.hs
+++ tests/M.hs
@@ -5,46 +5,39 @@ import System.FSNotify
 import Ives.ExampleGen.Conc
 import Ives.ExampleGen.Gen
 import Ives.ExampleGen.Exampler
-import System.INotify
 import System.IO
-import Control.Concurrent
 import Data.Typeable
-import Control.Monad
+import System.FilePath
 import Control.Exception
+import Control.Concurrent
 
 main :: IO ()
 main = do
   hSetBuffering stdout LineBuffering
-  manager <- startManager
-  stop <- watchDir manager "." actionFilter action
 
-  mPrev <- newEmptyMVar
-  mNew <- newEmptyMVar
   maybeExamples <- tryGetExamples "Foo.hs" "doh" (show $ typeOf doh) [] :: IO (Maybe ($(concretifyType 'doh), [Example]))
   let examples = case maybeExamples of
         Just (_, examples) -> examples
         Nothing -> []
-  stop <- watchDir manager dir actionFilter (action mPrev mNew)
-  catch (loop mPrev mNew examples)
-    (\e -> if e == UserInterrupt
-             then putStrLn "fuck"
-             else throwIO e)
 
-loop :: MVar [Example] -> MVar [Example] -> [Example] -> IO ()
-loop mPrev mNew examples = do
-  putMVar mPrev examples
-  newExamples <- takeMVar mNew
-  loop mPrev mNew newExamples
+  mNotify <- newEmptyMVar
+  manager <- startManager
+  stop <- watchDir manager "." actionFilter (action mNotify)
+  loop mNotify examples
+
+loop :: MVar () -> [Example] -> IO ()
+loop mNotify prevExamples = do
+  _ <- takeMVar mNotify
+  maybeExamples <- tryGetExamples "Foo.hs" "doh" (show $ typeOf doh) prevExamples :: IO (Maybe ($(concretifyType 'doh), [Example]))
+  case maybeExamples of
+    Just (_, examples) -> loop mNotify examples
+    Nothing -> loop mNotify prevExamples
   
 actionFilter :: Event -> Bool
 actionFilter (Modified path _) = takeFileName path == "Foo.hs"
 actionFilter _ = False
 
-action :: MVar [Example] -> MVar [Example] -> Event -> IO ()
-action mPrev mNew _ = do
-  prevExamples <- takeMVar mPrev
-  maybeExamples <- tryGetExamples "Foo.hs" "doh" (show $ typeOf doh) prevExamples :: IO (Maybe ($(concretifyType 'doh), [Example]))
-  case maybeExamples of
-    Just (_, examples) -> putMVar mNew examples
-    Nothing -> putMVar mNew prevExamples
+action :: MVar () -> Event -> IO ()
+action mNotify _ = do
+  putMVar mNotify ()
 
